3c4e3d0c0b6439fb646e817ff8edee25b03ae754
Signature Security Specialist <sigsecspec@gmail.com>
2025-09-24 13:13:29 -0700

    Update particles.js

--- public/particles.js
+++ public/particles.js
@@ -51,7 +51,7 @@ class ParticleSystem {
     }
 
     createParticles() {
-        const particleCount = Math.floor((window.innerWidth * window.innerHeight) / 35000);
+        const particleCount = Math.floor((window.innerWidth * window.innerHeight) / 100000);
         
         for (let i = 0; i < particleCount; i++) {
             this.particles.push({
@@ -59,7 +59,7 @@ class ParticleSystem {
                 y: Math.random() * this.canvas.height,
                 vx: (Math.random() - 0.5) * 0.2,
                 vy: (Math.random() - 0.5) * 0.2,
-                size: Math.random() * 120 + 40, // Different sizes from 40-160 pixels
+                size: Math.random() * 360 + 120, // Different sizes from 120-480 pixels
                 opacity: Math.random() * 0.4 + 0.1,
                 rotation: Math.random() * Math.PI * 2,
                 rotationSpeed: (Math.random() - 0.5) * 0.01,
@@ -119,7 +119,7 @@ class ParticleSystem {
             this.ctx.translate(particle.x, particle.y);
             this.ctx.rotate(particle.rotation);
             
-            const currentSize = particle.size + Math.sin(particle.pulse) * 6;
+            const currentSize = particle.size + Math.sin(particle.pulse) * 18;
             const currentOpacity = particle.opacity + Math.sin(particle.pulse) * 0.05;
             
             if (this.patchImage && this.patchImage.complete) {
